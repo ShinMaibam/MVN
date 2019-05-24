e1cdbb358d5e87568f159393980bd33125913cae
Shin Maibam <shinmaibam@Shins-MacBook-Pro.local>
2019-05-24 09:21:51 +0530

    updated the file content

--- Test.java
+++ Test.java
@@ -2,10 +2,11 @@ package com.pri.utilityHelper;
 
 public class Test {
 	public static void main(String[] args) {
-		//System.setProperty("webdriver.chrome.driver","/Documents⁩/⁨Automation⁩/⁨Tools/chromedriver⁩/");
-		String p = "TC01";
+		System.setProperty("====My First Program====");	
+System.setProperty("webdriver.chrome.driver","/Documents⁩/⁨Automation⁩/⁨Tools/chromedriver⁩/");
+		String p = "TC0001";
 		String filepath = "/Users/shinmaibam/Documents/Automation/TestRun/"+p+"";
-		System.out.println(filepath);
+		System.out.println("File Path is:" + filepath);
 		
 	}
 }
