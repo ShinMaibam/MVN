488f201b19095063690b9c55dead2f19c7429eaa
Shin Maibam <shinmaibam@Shins-MacBook-Pro.local>
2019-05-24 09:18:56 +0530

    adding a test file

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.pri.utilityHelper;
+
+public class Test {
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.chrome.driver","/Documents⁩/⁨Automation⁩/⁨Tools/chromedriver⁩/");
+		String p = "TC01";
+		String filepath = "/Users/shinmaibam/Documents/Automation/TestRun/"+p+"";
+		System.out.println(filepath);
+		
+	}
+}
