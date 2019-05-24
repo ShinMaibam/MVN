b3267c3b6ff9d0d640a6122fd78832b31cc534bf
Shin Maibam <shinmaibam@Shins-MacBook-Pro.local>
2019-05-24 09:29:53 +0530

    Updated file again

--- Test.java
+++ Test.java
@@ -2,11 +2,19 @@ package com.pri.utilityHelper;
 
 public class Test {
 	public static void main(String[] args) {
-		System.setProperty("====My First Program====");	
-System.setProperty("webdriver.chrome.driver","/Documents⁩/⁨Automation⁩/⁨Tools/chromedriver⁩/");
+		//Added my reference
+		System.setProperty("webdriver.chrome.driver","/Documents⁩/⁨Automation⁩/⁨Tools/chromedriver⁩/");
+		//Test Cases Name
 		String p = "TC0001";
+		//String Concatenation
 		String filepath = "/Users/shinmaibam/Documents/Automation/TestRun/"+p+"";
 		System.out.println("File Path is:" + filepath);
 		
 	}
+
+	public void myStr(){
+		System.setProperty("New java method");
+	}
+
 }
+
